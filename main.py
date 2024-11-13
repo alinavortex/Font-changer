9606d538e2c7f7f3962c151be0eb655b236c6c52
alinavortex <Alina_Azarova2005@mail.ru>
2024-11-13 13:35:12 +0300

    добавили импорты в main

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 ##стартовый модуль проекта 
-# TODO - import <зависимость из задания>
+
+# TODO - преименовать func1, func2
+from functions import func1, func2
+
 def main():
     # TODO - сделай вызов функций и functions
     pass
+
 #инициализационный скрипт
 if __name__=="__main__":
     main()
