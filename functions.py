a9c5b8568d3e8e6b1a23588eedce59a0c6a573a1
Alina_vortex <Alina_Azarova2005@mail.ru>
2024-11-27 13:52:38 +0300

    написала корявую первую часть фанкции

--- functions.py
+++ functions.py
@@ -1,7 +1,23 @@
 ##functions стартовый модуль для импорта проекта
 
+from docx import Document
+
+#извлекает название товара из документа
+def extract_product_names(path):
+
+# создание документа
+    document = Document(file_path)
+    if len(doc.paragraphs) > 0:
+        return
+
+
+# открытие документа
+#document = Document('/path/to/document.docx')
+
+
+
 def func1(n):
-    """функция 1"""
+    """функция 1. извлечение названия товаров"""
     pass
 
 
